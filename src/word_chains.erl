3afdf19ed9aaec3340d26b2e2b3d945b869fca3d
Graham Hay <grahamrhay@gmail.com>
2018-12-30 14:45:58 +0000

    Add property for next word

--- src/word_chains.erl
+++ src/word_chains.erl
@@ -1,7 +1,10 @@
 -module(word_chains).
 
--export([word_list/0]).
+-export([word_list/0, next_word/2]).
 
 word_list() ->
     {ok, Data} = file:read_file("words.txt"),
-    binary:split(Data, [<<"\n">>], [global]).
+    lists:map(fun(W) -> binary_to_list(W) end, binary:split(Data, [<<"\n">>], [global])).
+
+next_word(_FirstWord, LastWord) ->
+    LastWord.
--- test/prop_word_chains.erl
+++ test/prop_word_chains.erl
@@ -3,11 +3,27 @@
 -include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
-prop_word_chains() ->
-    ?FORALL(_, term(),
+prop_next_word_should_be_new() ->
+    ?FORALL({FirstWord, LastWord}, valid_words(),
         begin
-            true
+            io:format("First word: ~p, Last word: ~p", [FirstWord, LastWord]),
+            NextWord = word_chains:next_word(FirstWord, LastWord),
+            io:format("Next word: ~p", [NextWord]),
+            NextWord =/= FirstWord
         end).
+
+valid_words() ->
+    ?SUCHTHAT({FirstWord, LastWord},
+        ?LET(N, choose(2, 10),
+            begin
+                WordList = word_chains:word_list(),
+                SameLengthWords = lists:filter(fun(W) -> length(W) =:= N end, WordList),
+                {random_word(SameLengthWords), random_word(SameLengthWords)}
+            end),
+    FirstWord =/= LastWord).
+
+random_word(Words) ->
+    lists:nth(rand:uniform(length(Words)), Words).
 
 word_list_test() ->
     WordList = word_chains:word_list(),
@@ -16,9 +32,9 @@ word_list_test() ->
 word_list_first_word_test() ->
     WordList = word_chains:word_list(),
     FirstWord = lists:nth(1, WordList),
-    ?assertEqual(FirstWord, <<"aa">>).
+    ?assertEqual(FirstWord, "aa").
 
 word_list_last_word_test() ->
     WordList = word_chains:word_list(),
     LastWord = lists:last(WordList),
-    ?assertEqual(LastWord, <<"zzzs">>).
+    ?assertEqual(LastWord, "zzzs").
