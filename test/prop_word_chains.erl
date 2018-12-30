59dd5cbe39fa19be42ea5d8995f6cf7c4e06e501
Graham Hay <grahamrhay@gmail.com>
2018-12-30 13:43:16 +0000

    Get word list

--- test/prop_word_chains.erl
+++ test/prop_word_chains.erl
@@ -1,9 +1,24 @@
 -module(prop_word_chains).
 
 -include_lib("proper/include/proper.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 prop_word_chains() ->
     ?FORALL(_, term(),
         begin
             true
         end).
+
+word_list_test() ->
+    WordList = word_chains:word_list(),
+    ?assertEqual(length(WordList), 274926).
+
+word_list_first_word_test() ->
+    WordList = word_chains:word_list(),
+    FirstWord = lists:nth(1, WordList),
+    ?assertEqual(FirstWord, <<"aa">>).
+
+word_list_last_word_test() ->
+    WordList = word_chains:word_list(),
+    LastWord = lists:last(WordList),
+    ?assertEqual(LastWord, <<"zzzs">>).
